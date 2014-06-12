be35feb8405948378975ef16c915cc17bc32310f
mawiseman <mark@wiseman.net.au>
2014-06-13 09:37:31 +1000

    fixed port package import for Package Control

--- projecttypes/__init__.py
+++ projecttypes/__init__.py
@@ -1,20 +1,25 @@
 import os, glob
 
+
+#Temporarily commented out
+#I would like it to automaticly import all *.py from this folder
+#but when using Package Control the package is a zip file so this code won't work
+
 #Import all of the files in this folder
 
-pyImportFolder = os.path.dirname(os.path.realpath(__file__)) + "\*.py"
-
-for i in glob.glob(pyImportFolder):
-	pyPath, pyFileName = os.path.split(i)
-
-	if pyFileName == "__init__.py":
-		continue
-
-	try:
-		print("ProjectPorts Importing: %s" %pyFileName)
-		exec_command = "from .%s import *" %pyFileName.replace('.py', '')
-		exec(exec_command)
-
-	except ImportError as exc:
-		print("ProjectPorts Error: failed to import settings module ({})".format(exc))
+#pyImportFolder = os.path.dirname(os.path.realpath(__file__)) + "\*.py"
+#
+#for i in glob.glob(pyImportFolder):
+#	pyPath, pyFileName = os.path.split(i)
+#
+#	if pyFileName == "__init__.py":
+#		continue
+#
+#	try:
+#		print("ProjectPorts Importing: %s" %pyFileName)
+#		exec_command = "from .%s import *" %pyFileName.replace('.py', '')
+#		exec(exec_command)
+#
+#	except ImportError as exc:
+#		print("ProjectPorts Error: failed to import settings module ({})".format(exc))
 
--- sublime_events.py
+++ sublime_events.py
@@ -1,6 +1,10 @@
 import os, sublime, sublime_plugin  
 from .projecttypes import common
 
+#Temporarily added ProjectType imports here. See projecttypes.__init__.py for more details
+from .projecttypes import visual_studio
+
+
 print("ProjectPorts Loaded: %i" % len(common.ProjectPortManager.project_port_extensions))
 
 class EventDump(sublime_plugin.EventListener):  
